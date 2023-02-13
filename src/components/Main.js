94c6fb93e3d56c86710d772c6f5b6265fa671309
Marina Malyarchuk <pchupchu@yandex.ru>
2023-02-13 22:01:25 +0300

    Получить данные пользователя

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,38 @@
-//import { useState } from 'react';
+import React from 'react';
 import kusto from '../images/kusto.jpg';
 import ImagePopup from './ImagePopup';
+import { api } from '../utils/Api';
 
 function Main({ onEditAvatar, onEditProfile, onAddPlace }) {
+  const [userName, setUserInfo] = React.useState('Жак Ив Кусто');
+  const [userDescription, setUserDescription] = React.useState('Исследователь океана');
+  const [userAvatar, setUserAvatar] = React.useState(`${kusto}`);
+
+
+  React.useEffect(() => {
+    api.getProfileInfo()
+    .then((res) => {
+      console.log(res);
+      setUserInfo(res.name);
+      setUserDescription(res.about);
+      setUserAvatar(res.avatar);
+    })
+    
+  }, [])
+
   
   return (
     <main className="content">
     <section className="profile">
       <div className="profile__card">
         <div className="profile__avatar-container">
-          <img className="profile__avatar" src={kusto} alt="Аватар" />
+          <img className="profile__avatar" src={userAvatar} alt="Аватар" />
           <div className="profile__hover-avatar" onClick={onEditAvatar}></div>
         </div>
         <div className="profile__info">
-          <h1 className="profile__name">Жак-Ив Кусто</h1>
+          <h1 className="profile__name">{userName}</h1>
           <button className="profile__edit-button" type="button" onClick={onEditProfile}></button>
-          <p className="profile__description">Исследователь океана</p>
+          <p className="profile__description">{userDescription}</p>
         </div>
       </div>
       <button className="profile__add-button" type="button" onClick={onAddPlace}></button>
---
+++ src/utils/utils.js
@@ -0,0 +1,34 @@
+export const popups = document.querySelectorAll('.popup');
+export const popupCloseBtns = document.querySelectorAll('.popup__close-button');
+
+export const avatar = document.querySelector('.profile__avatar-container');
+export const formElementAvatar = document.querySelector('.form_avatar');
+export const popupAddAvatar = document.querySelector('.popup_type_add-avatar');
+
+export const btnOpenEditing = document.querySelector('.profile__edit-button');
+export const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+
+export const profileName = document.querySelector('.profile__name');
+export const profileDesc = document.querySelector('.profile__description');
+export const profileAvatar = document.querySelector('.profile__avatar');
+
+export const formElementProfile = document.querySelector('.form_profile');
+
+export const btnOpenAdding = document.querySelector('.profile__add-button');
+export const popupAddImage = document.querySelector('.popup_type_add-image');
+
+export const popupImage = document.querySelector('.popup_type_image');
+export const popupImageTitle = document.querySelector('.popup__image-title');
+export const popupImageCard = document.querySelector('.popup__image-card');
+
+export const formElementCard = document.querySelector('.form_card');
+export const popupDeleteCard = document.querySelector('.popup_type_delete-card');
+
+export const settings = {
+  formSelector: '.form',
+  inputSelector: '.form__item',
+  submitButtonSelector: '.form__button',
+  inactiveButtonClass: 'form__button_inactive',
+  inputErrorClass: 'form__item_type_error',
+  errorClass: 'form__item-error_active'
+};
